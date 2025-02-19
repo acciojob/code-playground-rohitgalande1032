21b78fa26f20b2c0cd9f01856dc231466a2041d6
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-19 09:41:27 +0530

    Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import PrivateRoute from "./PrivateRoute";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 const App = () => {
+  const [login, setLogin] = useState(false);
+
+  const handleClick = ()=> {
+    setLogin(!login)
+  }
+
   return (
+    <BrowserRouter>
     <div>
-        {/* Do not remove the main div */}
+        <p>{login ? "Logged in, Now you can enter Playground" : "You are not anthenticated, Please login first"}</p>
+        <nav>
+          <ul>
+            <li><Link to="/play-ground">PlayGround</Link></li>
+            <li><Link to="/">Login</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+            <Route path="/" element={<p>Login</p>}/>
+            <Route 
+            path="/play-ground" 
+            element={<PrivateRoute login={login} />}
+          />
+        </Routes>
+        <button onClick={handleClick}>{login ? "Logout" : "Login"}</button>
     </div>
+    </BrowserRouter>
   )
 }
 
